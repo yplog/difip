d14f54d546282c113b952a37631e6d6de0ddb103
yp <yalinpala@protonmail.com>
2020-01-06 16:21:14 +0300

    rootPath

--- src/main.go
+++ src/main.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 )
 
-var rootPath string = "/Users/yp/Documents/workspace/qooqapp/qooqmobi/src/"
+var rootPath string = "rootPath"
 var fileList = list.New()
 var line int = 1
 
